68d57e58f624ab2236f3d53492eef8d41300359b
fior <marcin.lukasz.fiorek@gmail.com>
2022-06-26 17:30:51 +0200

    Add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className='h-screen flex justify-center items-center bg-slate-800'>
+    <div className='h-screen flex justify-center items-center bg-teal-900'>
       <p className='text-6xl text-slate-300'>fior-plants 🌱</p>
     </div>
   );
