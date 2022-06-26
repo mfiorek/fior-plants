3a8eeee5a72d2cb797ca13856125a05c508d054f
fior <marcin.lukasz.fiorek@gmail.com>
2022-06-26 21:41:29 +0200

    Subscribe to user data and display it on HomePage or show Loader

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,38 @@
+import { useEffect, useState } from 'react';
+import { doc, DocumentData, onSnapshot } from 'firebase/firestore';
+import { database } from '../firebase';
 import { withAuthCheck } from '../components/withAuthCheck';
 import { useAuth } from '../contexts/AuthContext';
 import NavBar from '../components/NavBar';
+import Loader from '../components/Loader';
 
 function HomePage() {
-  const auth = useAuth();
+  const { currentUser } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
+  const [userData, setUserData] = useState<DocumentData | undefined>(undefined);
+
+  // Subscribe to user data
+  useEffect(() => {
+    const userUnsubscribe = onSnapshot(doc(database, `users/${currentUser?.uid}`), (userData) => {
+      setUserData(userData.data());
+      setIsLoading(false);
+    });
+    return userUnsubscribe;
+  }, []);
 
   return (
     <div className='flex min-h-screen flex-col bg-teal-800'>
       <NavBar />
-      <div className='flex grow flex-col items-center justify-center gap-8'>
-        <p className='text-6xl text-slate-300'>fior-plants 🪴</p>
-        <p className='text-4xl text-slate-300'>Home page</p>
-        <p className='text-4xl text-slate-300'>{auth.currentUser?.email}</p>
-      </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className='flex grow flex-col items-center justify-center gap-4'>
+          <p style={{fontSize: '20rem'}}>🪴</p>
+          <p className='text-4xl text-slate-300'>Hi {userData?.name} 👋</p>
+          <p className='text-4xl text-slate-300'>There is nothing here just yet...</p>
+          <p className='text-4xl text-slate-300'>...but will be coming soon!</p>
+        </div>
+      )}
     </div>
   );
 }
