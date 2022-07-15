b53883b3bee3f5ac012ce76692990895ca51b76e
fior <marcin.lukasz.fiorek@gmail.com>
2022-07-15 22:54:27 +0200

    Add ImageUploader progress bar

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 import { database, storage } from '../firebase';
 import { doc, updateDoc } from 'firebase/firestore';
@@ -11,6 +11,7 @@ type ImageUploaderProps = {
 
 const ImageUploader: React.FC<ImageUploaderProps> = ({ text, plantId }) => {
   const { currentUser } = useAuth();
+  const [progress, setProgress] = useState<number | null>(null);
   const storageRef = ref(storage, `${currentUser?.uid}/${plantId}`);
 
   const handleFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,24 +20,38 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ text, plantId }) => {
     uploadTask.on(
       'state_changed',
       (snapshot) => {
-        // TODO progress bar?
-        // const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        // console.log('Upload is ' + progress + '% done');
+        const uploadingProgress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(uploadingProgress);
       },
       (error) => {
         console.error(error);
       },
       () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          updateDoc(doc(database, `users/${currentUser?.uid}/plants/${plantId}`), { imgSrc: downloadURL });
-        });
+        getDownloadURL(uploadTask.snapshot.ref)
+          .then((downloadURL) => {
+            updateDoc(doc(database, `users/${currentUser?.uid}/plants/${plantId}`), { imgSrc: downloadURL });
+          })
+          .finally(() => {
+            setProgress(null);
+          });
       },
     );
   };
 
+  if (progress != null) {
+    return (
+      <div className='flex flex-col items-center gap-2'>
+        <p>
+          Uploading image <span className='font-bold'>{progress.toFixed()}%</span>
+        </p>
+        <progress className='progress progress-info w-96' value={progress} max='100'></progress>
+      </div>
+    );
+  }
+
   return (
     <>
-      <input id='file' type='file' capture="environment" accept="image/*" className='hidden' onChange={handleFileInput} />
+      <input id='file' type='file' capture='environment' accept='image/*' className='hidden' onChange={handleFileInput} />
       <label htmlFor='file'>
         <a className='btn btn-success cursor-pointer'>{text}</a>
       </label>
