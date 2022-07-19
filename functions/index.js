6d5e839146aada2a0ca4432f25ffa9ef9e36bdf5
fior <marcin.lukasz.fiorek@gmail.com>
2022-07-19 16:07:08 +0200

    Server-side function deletePlant for recursive collection delete

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,18 @@
-// const functions = require("firebase-functions");
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.deletePlant = functions.firestore
+    .document("users/{userId}/plants/{plantId}")
+    .onDelete((snap, context) => {
+      return admin.firestore().recursiveDelete(snap.ref)
+          .then(() => {
+            functions.logger.log("Succes deleting", context.params.plantId);
+            return {message: "Succes"};
+          })
+          .catch((e) => {
+            functions.logger.log("Error deleting", context.params.plantId, e);
+            return {message: `Error ${e}`};
+          });
+    });
